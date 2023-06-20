d1354502493d6e109999133492f9c1235589aa82
Kevin <kevin.haraux@gmail.com>
2023-06-20 09:17:45 +0200

    admin

---
+++ script.js
@@ -0,0 +1,16 @@
+function toggleImages() {
+    var imagesDiv = document.getElementById("images");
+    var utilisateursDiv = document.getElementById("utilisateurs");
+
+    imagesDiv.style.display = "block";
+    utilisateursDiv.style.display = "none";
+}
+
+function toggleUsers() {
+    var imagesDiv = document.getElementById("images");
+    var utilisateursDiv = document.getElementById("utilisateurs");
+
+    imagesDiv.style.display = "none";
+    utilisateursDiv.style.display = "block";
+}
+
